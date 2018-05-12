8f0b327a5fdda0f4941943337877626c729077ec
Adam M Duff <fikosan@gmail.com>
2018-05-12 12:31:09 -0400

    feat: Placeholder score now displayed

--- js/app.js
+++ js/app.js
@@ -19,8 +19,8 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x += this.spd * dt;
     if (this.x > 600) {
-        this.x = enemyCols[Math.floor(Math.random() * enemyCols.length)];
-        this.y = enemyRows[Math.floor(Math.random() * enemyRows.length)];
+        this.x = enemyCols[randLoc(enemyCols)];
+        this.y = enemyRows[randLoc(enemyRows)];
     }
     if (this.y === player.y && this.x >= player.x - 61 && this.x <= player.x + 30) {
         player.x = 201;
@@ -31,6 +31,8 @@ Enemy.prototype.update = function(dt) {
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.font = "20px 'Press Start 2P'";
+    ctx.fillText(score, 0,30);
 };
 
 // Now write your own player class
@@ -46,6 +48,7 @@ Player.prototype.update = function() {
     if (this.y === -15) {
         this.x = 201;
         this.y = 400;
+        gameNum += 1;
     }
 
 };
@@ -65,22 +68,43 @@ Player.prototype.handleInput = function(keyPressed) {
         this.x += 101;
     }
 }
-// Now instantiate your objects.
+// Now instantiate your objectsrandLoc(enemyCols).
 // Place all enemy objects in an array called allEnemies
 // Now write your own player class
 // Place the player object in a variable called player
-let player = new Player(201, 400);
+function randLoc(array) {
+    return Math.floor(Math.random() * array.length);
+}
+
+const player = new Player(201, 400);
 const enemyRows = [68, 151, 234];
 const enemyCols = [-800, -600, -500, -400, -300, -200, -100];
+let realScore = 0;
+let score = '0000' + realScore;
+let gameNum = 1;
 
 let allEnemies = [
-    new Enemy(enemyCols[Math.floor(Math.random() * enemyCols.length)], enemyRows[Math.floor(Math.random() * enemyRows.length)], 100), 
-    new Enemy(enemyCols[Math.floor(Math.random() * enemyCols.length)], enemyRows[Math.floor(Math.random() * enemyRows.length)], 100), 
-    new Enemy(enemyCols[Math.floor(Math.random() * enemyCols.length)], enemyRows[Math.floor(Math.random() * enemyRows.length)], 100),
-    new Enemy(enemyCols[Math.floor(Math.random() * enemyCols.length)], enemyRows[Math.floor(Math.random() * enemyRows.length)], 100),
+    new Enemy(enemyCols[randLoc(enemyCols)], enemyRows[randLoc(enemyRows)], 100), 
+    new Enemy(enemyCols[randLoc(enemyCols)], enemyRows[randLoc(enemyRows)], 100), 
+    new Enemy(enemyCols[randLoc(enemyCols)], enemyRows[randLoc(enemyRows)], 100),
+    new Enemy(enemyCols[randLoc(enemyCols)], enemyRows[randLoc(enemyRows)], 100)
 ];
 
+//TODO: Add gems/hearts
 
+//TODO: Add hearts at to of screen to keep track of lives remaining
+
+//TODO: Add score function, keep track of score on top left of screen
+    // Score should go up each time player reaches water
+    // Score multiplier should go up each time player reaches water
+    // Enemy speed should go up each time player reaches water
+    // 4th enemy is a "rogue" and will not appear until a few "games" in, and
+    // his speed is much faster than the others.
+
+    
+    
+
+randLoc(enemyCols)
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
